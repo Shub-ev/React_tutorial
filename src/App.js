50a0147050bcdd95927e82d8a253fd95daf6c189
shubhamg <shubhamg200204@gmail.com>
2024-07-22 23:41:02 +0530

    a

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Func_01 from './components/Func_01' // default import from Func_01 which 
 import { Func_02 } from './components/Func_02' //Func_02 is not default export hence it should be name specified
 import Class_01 from './components/Class_01'
 import Class_02 from './components/Class_02'
+import Func_03 from './components/Func_03'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Func_02/>
       <Class_01/>
       <Class_02/>
+      <Func_03 name="Shubham"/> {/* Passing props to the component */}
     </div>
   )
 }
---
+++ src/components/Func_03.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Func_03 = (props) => {
+  return (
+    <div>
+        <span>Hello {props.name}</span>
+    </div>
+  )
+}
+
+export default Func_03
