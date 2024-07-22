c764c5c5e53c1f9a9cd1858d4b435900f89e078e
shubhamg <shubhamg200204@gmail.com>
2024-07-22 23:43:34 +0530

    a

--- src/components/Func_03.js
+++ src/components/Func_03.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Func_03 = (props) => {
+const Func_03 = (props) => { // accepting props from parent
   return (
     <div>
-        <span>Hello {props.name}</span>
+        <span>Hello {props.name}</span> {/* using the props accepted from the parent */}
     </div>
   )
 }
